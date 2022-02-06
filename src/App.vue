9eca2000ddb7e82b3454b637b6205eba0951c0a9
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-02-06 01:09:26 -0500

    Init project
    - Añade estructura básica para hacer el ejercicio práctico de la clase

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <PersonalForm />
+</template>
+
+<script>
+import PersonalForm from "./components/PersonalForm.vue";
+export default {
+  name: "App",
+  components: {
+    PersonalForm,
+  },
+  data() {
+    return {
+      name: "Daniel",
+      age: 18,
+      color: "#09F",
+    };
+  },
+};
+</script>
---
+++ src/components/PersonalForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <article class="content">
+    <form class="form">
+      <div class="control">
+        <label for="name">Nombre</label>
+        <input type="text" placeholder="Ingresa tu nombre" />
+      </div>
+      <div class="control">
+        <label for="age">Edad</label>
+        <input type="number" placeholder="Ingresa tu edad..." />
+      </div>
+      <div class="control">
+        <label for="color">Color favorito</label>
+        <input type="color" />
+      </div>
+    </form>
+  </article>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.content {
+  max-width: 1280px;
+  width: 100%;
+  padding: 1rem;
+  margin: 0 auto;
+}
+.form {
+  margin: 0 auto;
+  width: 75%;
+}
+</style>
